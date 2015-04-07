686a46f802ae993fa0a8bc222ebd70b8fe83be4f
Gonza Gluzman <dggluz@gmail.com>
2015-04-07 14:13:22 -0300

    Sincranizado todo con los sources

--- src/participacion-popular/js/script_prie.js
+++ src/participacion-popular/js/script_prie.js
@@ -1,41 +1,38 @@
-document.createElement('video');
-document.createElement('audio');
-document.createElement('track');
-
-
-
-$(document).ready(function() {
-	var myPlayer_1;
-	var myPlayer_2;
+sources.ready(function() {
+	console.log('se llama');
+	var myPlayer_1,
+		myPlayer_2,
+		width_reproductor,
+		height_reproductor;
 	
-	if( $(window).height() <= 442 && $(window).width() <=1022 )
-	{
-		var width_reproductor = 350;
-		var height_reproductor = 150;
+	if($(window).height() <= 442 && $(window).width() <=1022) {
+		width_reproductor = 350;
+		height_reproductor = 150;
 	}
-	else
-	{
-		var width_reproductor = 400;
-		var height_reproductor = 175;
+	else {
+		width_reproductor = 400;
+		height_reproductor = 175;
 	}
 	
-	$('#example_video_1').attr( "width", width_reproductor );
-	$('#example_video_1').attr( "height", height_reproductor );
+	$('#example_video_1')
+		.attr('width', width_reproductor)
+		.attr('height', height_reproductor);
 	
-	$('#example_video_2').attr( "width", width_reproductor );
-	$('#example_video_2').attr( "height", height_reproductor );
+	$('#example_video_2')
+		.attr('width', width_reproductor)
+		.attr('height', height_reproductor);
 	
-	videojs("example_video_1").ready(function(){
+	videojs('example_video_1').ready(function(){
 		myPlayer_1 = this;
 		$('#example_video_1').find('.vjs-big-play-button').remove();
 	});
 
-	videojs("example_video_2").ready(function(){
+	videojs('example_video_2').ready(function(){
 		myPlayer_2 = this;
 		$('#example_video_2').find('.vjs-big-play-button').remove();
 	});
 	
-	videojs("example_video_1").on("firstplay", function(){
+	videojs('example_video_1').on('firstplay', function(){
 		myPlayer_1.addClass('vjs-fullscreen');
 
 			var button = myPlayer_1.controlBar.addChild('button', {
@@ -49,34 +46,33 @@ $(document).ready(function() {
 			};
 	});
 
-		videojs("example_video_1").on("ended", function(){
-			this.removeClass('vjs-fullscreen');
-		});
-
-	
-	videojs("example_video_2").on("firstplay", function(){
-		  myPlayer_2.addClass('vjs-fullscreen');
-
-			var button = myPlayer_2.controlBar.addChild('button', {
-				text: 'alternar'
-			});
-
-			button.addClass('exit-button');
-
-			button.el().onclick = function() {
-				$(myPlayer_2.el()).toggleClass('vjs-fullscreen');
-			};
+	videojs('example_video_1').on('ended', function(){
+		this.removeClass('vjs-fullscreen');
 	});
 
-		videojs("example_video_2").on("ended", function(){
-			this.removeClass('vjs-fullscreen');
+	videojs('example_video_2').on('firstplay', function(){
+		  myPlayer_2.addClass('vjs-fullscreen');
+
+		var button = myPlayer_2.controlBar.addChild('button', {
+			text: 'alternar'
 		});
-	videojs("example_video_1").ready(function(){
+
+		button.addClass('exit-button');
+
+		button.el().onclick = function() {
+			$(myPlayer_2.el()).toggleClass('vjs-fullscreen');
+		};
+	});
+
+	videojs('example_video_2').on('ended', function(){
+		this.removeClass('vjs-fullscreen');
+	});
+	videojs('example_video_1').ready(function(){
 		setTimeout(function() {
 			$('.vjs-captions-button').find('.vjs-menu-item').eq(0).click();
 		}, 500);
 	});
-	videojs("example_video_2").ready(function(){
+	videojs('example_video_2').ready(function(){
 		setTimeout(function() {
 			$('.vjs-captions-button').find('.vjs-menu-item').eq(0).click();
 		}, 500);
--- src/participacion-y-representacion/js/script_info_2.js
+++ src/participacion-y-representacion/js/script_info_2.js
@@ -1,59 +1,56 @@
-document.createElement('video');
-document.createElement('audio');
-document.createElement('track');
+sources.ready(function() {
+	var myPlayer,
+		width_reproductor,
+		height_reproductor;
 
-
-
-$(document).ready(function() {
-
-	var myPlayer;
-
-	if( $(window).height() <= 442 && $(window).width() <=1022 )
-	{
-		var width_reproductor = 350;
-		var height_reproductor = 250;
+	if($(window).height() <= 442 && $(window).width() <=1022) {
+		width_reproductor = 350;
+		height_reproductor = 250;
 	}
-	else
-	{
-		var width_reproductor = 400;
-		var height_reproductor = 250;
+	else {
+		width_reproductor = 400;
+		height_reproductor = 250;
 	}
 	
-	$('#example_video_1').attr( "width", width_reproductor );
-	$('#example_video_1').attr( "height", height_reproductor );
+	$('#example_video_1').attr( 'width', width_reproductor );
+	$('#example_video_1').attr( 'height', height_reproductor );
 	
-	videojs("example_video_1").ready(function(){
+	videojs('example_video_1').ready(function(){
 		myPlayer = this;
 		$('#example_video_1').find('.vjs-big-play-button').remove();
 	});
 	
-	videojs("example_video_1").on("firstplay", function(){
+	videojs('example_video_1').on('firstplay', function() {
 	  myPlayer.addClass('vjs-fullscreen');
-
-			var button = myPlayer.controlBar.addChild('button', {
-				text: 'alternar'
-			});
-
-			button.addClass('exit-button');
-
-			button.el().onclick = function() {
-				$(myPlayer.el()).toggleClass('vjs-fullscreen');
-			};
-	});
-
-		videojs("example_video_1").on("ended", function(){
-			this.removeClass('vjs-fullscreen');
+		var button = myPlayer.controlBar.addChild('button', {
+			text: 'alternar'
 		});
 
-		videojs("example_video_1").on("error", function(){
-			document.location.reload();
-		});
+		button.addClass('exit-button');
 
-	$(document.body).on('click', '.anios-container a', function(e) {
-		e.preventDefault();
-		gui.Shell.openItem($(this).attr('href'));
+		button.el().onclick = function() {
+			$(myPlayer.el()).toggleClass('vjs-fullscreen');
+		};
 	});
-	videojs("example_video_1").ready(function(){
+
+	videojs('example_video_1').on('ended', function(){
+		this.removeClass('vjs-fullscreen');
+	});
+
+	videojs('example_video_1').on('error', function(){
+		document.location.reload();
+	});
+	if(typeof gui != 'undefined') {
+		$(document.body).on('click', '.anios-container a', function(e) {
+			e.preventDefault();
+			gui.Shell.openItem($(this).attr('href'));
+		});
+	}
+	else {
+		$('.anios-container a').attr('target', '_blank');
+	}
+
+	videojs('example_video_1').ready(function(){
 		setTimeout(function() {
 			$('.vjs-captions-button').find('.vjs-menu-item').eq(0).click();
 		}, 500);
